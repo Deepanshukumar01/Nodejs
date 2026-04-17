45065f458e8ec457622e78e6b9a556c764a5a6ae
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-04-17 23:57:54 +0530

    node js basic

---
+++ lec34/1.js
@@ -0,0 +1,21 @@
+//  ./ denotes that same folder 
+// topic 1
+/* dekho ye koi node js koi frame work library ya ye sb nhi hai 
+balki ye ek runtime environment hai jiski help se hm js ko brower ke bahr use 
+kr sakte hai */
+
+// topic 2
+/* terminal se node v se hme node js ka version milega same like others */
+
+// topic 4 REPL(read evaluate print loop)
+/* ab terminal pe node type krne se hm node ke world mei aa jate hai udr normal 
+terminal commands work nhi krti like pwd cd etc
+pr ha node js ek tarike se js hi to ye 2+3 or ye sb kaam krne lgega ab 
+
+jaise udr window hota tha bese hi idr global hota hai because ab window to console 
+mei hoti thi na
+.help command bhi hoti hai jo help krti hai in sb ke liye
+or ha node se bahr ke liye 2 time control c ya .exit*/
+
+// topic 5 (node se kaise file run hoti hai )
+/* node file name.js */
