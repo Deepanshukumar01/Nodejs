68a2dcc6b601d41aee730da8d63823e554170754
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-04-18 22:11:35 +0530

    export from other direc files

---
+++ lec34/8(fruit)/index.js
@@ -0,0 +1,8 @@
+let apple =require("./apple")
+let banana =require("./banana")
+let oranges =require("./orange")//yha jo likhte hai bo file name hota hai 
+
+let fruit =[apple,banana,oranges]
+
+module.exports = fruit
+
