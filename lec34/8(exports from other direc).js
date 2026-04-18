3c2ce1f5d221121fe3360acb2409671d577d201c
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-04-18 22:08:26 +0530

    export from other dir

---
+++ lec34/8(exports from other direc).js
@@ -0,0 +1,8 @@
+// dekho same folder mei to exports krna aasan hota hai 
+// pr other directory ke liye other directory mei jitni file unka content ek file mei export
+// kro than usi directory mei ek index.html wali file mei ye sb kro than 
+
+
+let data=require("./8(fruit)")
+
+console.log(data[0].name)
