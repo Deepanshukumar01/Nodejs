b1fe0c7b8098ebf16d5e385ec2c3070f2e4e3713
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-04-20 23:58:16 +0530

    import file

---
+++ lec34/12import.js
@@ -0,0 +1,19 @@
+// pahle sirf require hota tha pr uske kuch disadvantages the 
+// isliye hm. import bhi use krte hai pr yaad rakhna ya to import 
+//use krna ya fir require
+
+//jaise hi import hme choice deta hai kya kya hme 
+// require krna hai 
+
+
+
+
+import {a,b,c} from "./12.js";
+
+console.log(a)
+
+
+//pr ye line direct nhi hogi run uske liye 
+// pahle hme use krna hoga
+// step 1 jis folder mei import export kr rhe ho usme hi 
+// ek package bnao or .json mei "type":" pahle se hi hoga uske samne "module"likh do bs
