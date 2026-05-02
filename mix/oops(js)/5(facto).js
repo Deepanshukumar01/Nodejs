8b6291a1030d78a2cdcc1e3b24842bc4b4ddc744
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-05-02 22:32:30 +0530

    factory function

---
+++ mix/oops(js)/5(facto).js
@@ -0,0 +1,26 @@
+/* factory function se code to kafi reuseable bn gya or abhi bhi hr object ke liye alag
+se copy ja rhi hai 
+*/
+
+function Studentss(name,age){
+    const student={
+        name:name,
+        age:age,
+        talk:function(){
+            console.log(`${name} is speaking`)//agr function mei kuch bhi return nhi 
+            // bydefault undefined print hota hai 
+        }
+    };
+    return student;
+}
+
+let s1=Studentss("deepanshu",20)//isme ye problem hai ki hr object ke pass apni copy jati hai 
+let s2=Studentss("harshit",20)
+console.log(s1.talk===s2.talk)//false har object ke paas alag function copy hai
+
+// ye hi hai disadvantages ek object bnane ke baad bhi sabhi ka function alg alg store ho rha hai
+
+//console.log(s1.talk()===s2.talk()) // isme undefined=undefined to true hoga print
+
+
+// new keyword se hi this ki value window se change hoge function name ho jati hai 
